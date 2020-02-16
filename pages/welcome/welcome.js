1b1e651a9c74419c0f08ac7552f042512ad6dc6f
xuchaoyang <1034705317@qq.com>
2020-02-16 20:01:50 +0800

    2020-2-16号学习

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,15 @@
 Page({
-  
+  onTap:function(){
+    wx.redirectTo({
+      url: '../posts/posts',
+    })
+  },
+
+  onUnLoad:function(){
+    console.log('welcome page is unload');
+  },
+
+  onHide:function(){
+    console.log('welcome page is onhide');
+  }
 })
